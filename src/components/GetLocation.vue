aba035c9bad213ce1c233472941a8e16a301f990
Kelvin Njihia <kelvin2012kim@gmail.com>
2025-03-10 17:17:05 +0100

    Initial commit

---
+++ src/components/GetLocation.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { Ref } from 'vue';
+
+type GeoLocation = Ref<{
+  latitude: number;
+  longitude: number;
+}>;
+
+const coords: Ref<GeoLocation | undefined> = ref();
+const geolocationBlockedByUser: Ref<boolean> = ref(false);
+</script>
